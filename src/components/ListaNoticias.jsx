a3af7f435b8891ddbfb8bb5fca4c724823a5c43e
brunomry <brunomadozzo@gmail.com>
2024-02-18 07:35:15 -0300

    Agregue hook de use effect y tambien de useState para noticias, pais y categoria, ademas de añadir la funcion consultarAPI para traer las noticias de la api

--- src/components/ListaNoticias.jsx
+++ src/components/ListaNoticias.jsx
@@ -1,11 +1,17 @@
 import Noticia from "./Noticia";
 
-const ListadoNoticias = () => {
+const ListadoNoticias = ({ noticias}) => {
   return (
     <section className="sectionNews mx-auto my-5 rounded-2">
       <h3 className="py-3 text-center">Resultados</h3>
       <article className="py-5 border-top border-2 d-flex gap-3 flex-wrap justify-content-center px-2">
-        
+        {Array.isArray(noticias) && noticias.length > 0 ? (
+          noticias.map((noticia, index) => (
+            <Noticia key={index} noticia={noticia} />
+          ))
+        ) : (
+          <p className="text-center">No se encontraron resultados</p>
+        )}
       </article>
     </section>
   );
--- src/components/Noticia.jsx
+++ src/components/Noticia.jsx
@@ -1,24 +1,23 @@
 import { Button, Card } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const Noticia = () => {
+const Noticia = ({ noticia }) => {
   return (
     <Card className="cardNews pb-3 rounded-2">
-      <img
-        src=""
-        alt=""
-        className="imgNews rounded-top-2"
-      />
-      <div className="d-flex flex-column justify-content-between">
-        <div className="px-2 mb-3 mt-2 containerDescription">
-          <h6>creador</h6>
-          <h5>titulo de noticia</h5>
-          <p>descripción de noticia</p>
-        </div>
-        <Button className="btnSeeNews border-0 px-2 py-3 mx-2">
-          Ver noticia completa
-        </Button>
-      </div>
+      {noticia && (
+        <>
+          <img src={noticia["image_url"]} alt={noticia["title"]} className="imgNews rounded-top-2"/>
+          <div className="d-flex flex-column justify-content-between">
+          <div className="px-2 mb-3 mt-2 containerDescription">
+            <h6>{noticia["creator"]}</h6>
+            <h5>{noticia["title"]}</h5>
+            <p>{noticia["description"]}</p>
+          </div>
+          <Button className="btnSeeNews border-0 px-2 py-3 mx-2">Ver noticia completa</Button>
+          </div>
+          
+        </>
+      )}
     </Card>
   );
 };
